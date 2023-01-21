cec379fb11a88217331d95d2e998d2d0db9c974a
Anna-Mamedova <mamedi289@gmail.com>
2023-01-21 18:39:50 +0200

    feat: add brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('')
+  if(str.length % 2 !== 0){
+      return false
+  }
+
+  for(let i = 1; i < str.length; i++){
+      for(let j = 0; j < bracketsConfig.length; j++){
+          if(str[i] === bracketsConfig[j][1] && str[i - 1] === bracketsConfig[j][0]){
+              str.splice(i - 1, 2)
+              i = 0
+              break
+          }
+      }
+  }
+  return str.length === 0
 }
